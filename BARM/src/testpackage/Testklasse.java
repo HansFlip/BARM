c3bd78703634ff9b6924f30b37bccf74dbb354bb
HansFlip <91021400+HansFlip@users.noreply.github.com>
2021-10-04 09:26:12 +0200

    First commit for testing purposes

---
+++ BARM/src/testpackage/Testklasse.java
@@ -0,0 +1,11 @@
+package testpackage;
+
+public class Testklasse {
+
+	public static void main(String[] args) {
+		System.out.println("Schau mal hier, das ist cool!");
+		int i = 1;
+		System.out.println("i ist: "+ i);
+	}
+
+}
---
+++ BARM/src/testpackage/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * @author eliasvollrath
+ *
+ */
+package testpackage;
